bc22fb9cc46cb6be174f3c3fc199ea0b2f47d33a
Daniel Neto <daniel@danielneto.dev.br>
2022-10-16 19:25:42 -0300

    implements downloading videos from YouTube

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { YoutubeDownloader } from './youtube-downloader.js';
+
+try {
+  const download = await new YoutubeDownloader().download(process.argv[2]);
+
+  console.log(download.message);
+
+  download.stream.on('end', () => console.log('Download completed sucessfully!'));
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  }
+}
